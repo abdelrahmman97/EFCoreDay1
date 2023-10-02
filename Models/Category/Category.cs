1072caedc88475a708c97c257b6329f9007afa6a
tyson <tysonosman@gmail.com>
2023-10-02 15:23:31 +0300

    updaate

---
+++ Models/Category/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreDay1.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public ICollection<Product> Products { get; set; } 
+        public string Catagory { get; set; }
+
+    }
+}
